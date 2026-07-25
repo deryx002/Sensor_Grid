21acfa50f4840ffe1c273cbedc7bbf563351b09a
Dharun S <dharunananth002@gmail.com>
2026-07-25 17:33:19 +0530

    Fix : Optimized UI

--- client/src/layouts/PublicLayout.jsx
+++ client/src/layouts/PublicLayout.jsx
@@ -1,9 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 
 import Navbar from "../components/layout/Navbar/Navbar";
 import Footer from "../components/layout/Footer/Footer";
 
 const PublicLayout = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <>
       <Navbar />
